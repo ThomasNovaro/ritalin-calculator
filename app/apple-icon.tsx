1fc2c78b0bbf4b7b3869928f5a48695af8d8347e
Thomas Novaro <hello@thomasnovaro.com>
2026-04-16 12:16:49 +0200

    Replace PokéMed icons with dose branding

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -13,70 +13,15 @@ export default function Icon() {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          background: "#050505", // The dark theme background
+          background: "#1a1a1a",
+          color: "#f5f0e8",
+          fontFamily: "sans-serif",
+          fontSize: "42px",
+          fontWeight: "normal",
+          letterSpacing: "-0.05em",
         }}
       >
-        <div
-          style={{
-            position: "relative",
-            width: "80px",
-            height: "140px",
-            borderRadius: "40px",
-            background: "#FAFAFA",
-            border: "12px solid #18181B",
-            display: "flex",
-            flexDirection: "column",
-            overflow: "hidden",
-            transform: "rotate(45deg)",
-          }}
-        >
-          {/* Top Half Red */}
-          <div
-            style={{
-              width: "100%",
-              height: "50%",
-              background: "#ef4444",
-              borderBottom: "12px solid #18181B",
-              boxSizing: "border-box",
-            }}
-          />
-          {/* Bottom Half White */}
-          <div
-            style={{
-              width: "100%",
-              height: "50%",
-              background: "#FAFAFA",
-            }}
-          />
-          
-          {/* Middle Button */}
-          <div
-            style={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              width: "48px",
-              height: "48px",
-              borderRadius: "24px",
-              background: "#FAFAFA",
-              border: "12px solid #18181B",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              boxSizing: "border-box",
-            }}
-          >
-            <div
-              style={{
-                width: "12px",
-                height: "12px",
-                borderRadius: "6px",
-                background: "#18181B",
-              }}
-            />
-          </div>
-        </div>
+        dose
       </div>
     ),
     { ...size }
--- app/icon.tsx
+++ app/icon.tsx
@@ -13,70 +13,15 @@ export default function Icon() {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          background: "transparent",
+          background: "#1a1a1a",
+          color: "#f5f0e8",
+          fontFamily: "sans-serif",
+          fontSize: "120px",
+          fontWeight: "normal",
+          letterSpacing: "-0.05em",
         }}
       >
-        <div
-          style={{
-            position: "relative",
-            width: "300px",
-            height: "460px",
-            borderRadius: "150px",
-            background: "#FAFAFA",
-            border: "40px solid #18181B",
-            display: "flex",
-            flexDirection: "column",
-            overflow: "hidden",
-            transform: "rotate(45deg)",
-          }}
-        >
-          {/* Top Half Red */}
-          <div
-            style={{
-              width: "100%",
-              height: "50%",
-              background: "#ef4444",
-              borderBottom: "40px solid #18181B",
-              boxSizing: "border-box",
-            }}
-          />
-          {/* Bottom Half White */}
-          <div
-            style={{
-              width: "100%",
-              height: "50%",
-              background: "#FAFAFA",
-            }}
-          />
-          
-          {/* Middle Button */}
-          <div
-            style={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              width: "140px",
-              height: "140px",
-              borderRadius: "70px",
-              background: "#FAFAFA",
-              border: "40px solid #18181B",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              boxSizing: "border-box",
-            }}
-          >
-            <div
-              style={{
-                width: "40px",
-                height: "40px",
-                borderRadius: "20px",
-                background: "#18181B",
-              }}
-            />
-          </div>
-        </div>
+        dose
       </div>
     ),
     { ...size }
